464b6c506026ef709f4048a238af4001f9b5b3e9
callmeVineeth <vineethpv024@gmail.com>
2024-03-04 14:54:47 +0530

    changes made

--- src/AnimatedRoutes.js
+++ src/AnimatedRoutes.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import HomePage from './Pages/Home/HomePage.jsx';
 import SkillsHome from './Pages/Skills/SkillsHome';
 import Projects from './Pages/Projects/Projects';
@@ -12,9 +12,10 @@ import Savora from './Project _Previews/Savora/Savora';
 import SpaceTour from './Project _Previews/SpaceTour/SpaceTour';
 import GlobalChronicle from './Project _Previews/GlobalChronicle/GlobalChronicle';
 import Edumentor from './Project _Previews/Edumentor/Edumentor';
-import {AnimatePresence} from 'framer-motion'
+// import {AnimatePresence} from 'framer-motion'
 import Navbar from './Pages/Navbar/Navbar';
 import About from './Pages/About/About.js';
+import Loading from './Pages/Loading/Loading.jsx';
 
 
 
@@ -23,16 +24,15 @@ function AnimatedRoutes() {
  const location = useLocation();
 
   return (
-    <AnimatePresence>
-         <Routes location={location} key={location.pathname}>
+    
+         <Routes>
               <Route path='/' element={
                 <>
                 <Navbar/>
-            
-                
+                <Loading/>
                <div className='sections' id='homeSection'><HomePage/>   </div>
                <div className='sections' id='aboutSection'> <About/>   </div>
-               <div className='sections' id='projectSection'><Projects/>   </div>
+               <div className='sections' id='projectSection'><Projects/></div>
                <div className='sections' id='skillSection'><SkillsHome/> </div>
                <div className='sections' id='contactSection'><Contact/>    </div>
             </>
@@ -46,7 +46,6 @@ function AnimatedRoutes() {
               <Route path='/eduMentor' element={<Edumentor/>}>  </Route>
             </Routes>
 
-    </AnimatePresence>
   )
 }
 
---
+++ src/Pages/Loading/Loading.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import spinner from './Loading.css'
+
+function Loading() {
+  return (
+    <div>
+        {/* Loading
+
+<button type="button" class="bg-indigo-500 ..." disabled>
+  <div className="animate-spin h-5 w-5 mr-3 ..." >
+  </div>
+  Processing...
+</button> */}
+
+
+    </div>
+  )
+}
+
+export default Loading
