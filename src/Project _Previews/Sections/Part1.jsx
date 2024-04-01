66c06fc564ccd1f09ba65940590aa68f0665c30d
callmeVineeth <vineethpv024@gmail.com>
2024-04-01 07:15:31 +0530

    changes made in the project section

--- src/Project _Previews/Sections/Part1.jsx
+++ src/Project _Previews/Sections/Part1.jsx
@@ -9,7 +9,7 @@ function Part1({ title, description, liveSite, gitCode , sitelabel='Live Site'})
 
 
   return (
-    <motion.div className='w-full md:w-1/3 flex flex-col justify-between p-5 text-black md:ml-32 bg-gray-100'
+    <motion.div className='w-full md:w-1/3 flex flex-col justify-between p-5 text-black md:ml-32 bg-red-100'
     initial={{ y:800, transition:{ duration: 0.9, delay:1} }}
     animate={{ y:0, transition: { duration: 0.9, ease: 'easeInOut' } }}
     exit={{  y:-800, transition: { duration: 1, ease: 'easeInOut'} }}
@@ -23,7 +23,7 @@ function Part1({ title, description, liveSite, gitCode , sitelabel='Live Site'})
         <div className='opacity-90 mt-1 md:mt-4'>{description}</div>
 
         <div className='mt-12 flex flex-col'>
-          <span className='text-md font-thin text-blue-900'>View the Project on GitHub:</span>
+          <span className='text-md text-blue-900'>View the Project on GitHub:</span>
           <a className='mt-4 flex flex-row gap-4 text-blue-600 w-44' target='_blank' href={liveSite}><IoIosLink size={23} />{sitelabel}</a>
           <a className='flex flex-row gap-4 text-blue-600 w-44' target='_blank' href={gitCode}><FaRegFileCode size={23} />Check Code</a>
         </div>
