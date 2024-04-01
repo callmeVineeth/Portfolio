8396e1565ae10e0a21601baf6174a14221c03547
callmeVineeth <vineethpv024@gmail.com>
2024-04-01 07:09:39 +0530

    overflowing links updated

--- src/Project _Previews/Sections/Part2.jsx
+++ src/Project _Previews/Sections/Part2.jsx
@@ -133,7 +133,7 @@ function Part2({ projectTitle,projectDesc,previewVideo1,previewVideo2,
             <span className=" font-bold text-2xl">Feedback</span>
             <div className=" opacity-70">
               In case you have any feedback or questions, 
-              feel free to reach at <span className=" cursor-pointer opacity-110 text-blue-800">https://github.com/callmeVineeth</span> and open a new issues on this repo or reach out to me 
+              feel free to reach at <a href="https://github.com/callmeVineeth" target="_blank" className=" cursor-pointer opacity-110 text-blue-800">https://github.com/callmeVineeth</a> and open a new issues on this repo or reach out to me 
               at <span className=" cursor-pointer opacity-110  text-blue-900">vineethpv024@gmail.com</span>
             </div>
             <span className=" font-extralight opacity-70">Thanks</span>
